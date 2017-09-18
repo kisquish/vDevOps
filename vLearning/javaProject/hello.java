5ac8bc1987a9ef25525100665c60ecd623982b87
kisquish <hvmiaf@gmail.com>
2017-09-18 17:08:51 +0200

    creating cicd compose stack

---
+++ vLearning/javaProject/hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class hello1 {
+	public static void main(String[] args){
+		System.out.println("Hello world");
+	}
+}
+
+RUN cd /path/to/directory (DEPREDICATED)
+COPY  src1/ /path/to/directory/src2
+
+WORKDIR /path/to/directory 
+COPY 
